f6e42cdc664e44b2490830d3911cc33ba151f322
zhanyuan <zhanyuan1@huawei.com>
2020-09-10 18:45:32 +0800

    change

---
+++ arm32/main.c
@@ -0,0 +1,122 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define UP_DIV(x, y) (((x) + (y) - (1)) / (y))
+#define UP_ROUND(x, y) (((x) + (y) - (1)) / (y) * (y))
+
+#define ROW 4
+#define COL 4
+#define DEPTH 1
+#define ROW4 UP_ROUND(ROW, 4)
+#define COL2 UP_ROUND(COL, 2)
+#define DEPTH16 UP_ROUND(DEPTH, 16)
+
+void MatmulInt8Neon32(const int8_t *a, const int8_t *b, int8_t *dst, int row, int col, int deep16,
+                      const int *input_sums, const int *weight_bias, int act_min, int act_max, int out_zp,
+                      int multiplier, int left_shift, int right_shift, int stride);
+
+void RowMajor2Row4x16Major(int8_t *src, int row, int col, int8_t *dst, int col_16) {
+  int stride = sizeof(int8_t) * 16 * 4;
+  for (int r = 0; r < row; ++r) {
+    for (int c = 0; c < col; ++c) {
+      int stride_n = r / 4 * (col_16 / 16) + c / 16;
+      int src_idx = r * col + c;
+      dst[stride * stride_n + r % 4 * 16 + c % 16] = src[src_idx];
+    }
+  }
+}
+
+void RowMajor2Col16x2Major(int8_t *src, int row, int col, int8_t *dst, int row_16) {
+  int stride = sizeof(int8_t) * 16 * 2;
+  for (int r = 0; r < row; ++r) {
+    for (int c = 0; c < col; ++c) {
+      int stride_n = c / 2 * (row_16 / 16) + r / 16;
+      int src_idx = r * col + c;
+      dst[stride * stride_n + c % 2 * 16 + r % 16] = src[src_idx];
+    }
+  }
+}
+
+// void RowMajor2Asums(int8_t *a, int row, int col, int b_zp, int *dst) {
+//   for (int r = 0; r < row; ++r) {
+//     for (int c = 0; c < col; ++c) {
+//       int src_idx = r * col + c;
+//       dst[r] += a[src_idx];
+//     }
+//     dst[r] *= b_zp;
+//   }
+// }
+
+// void RowMajor2Bbias(int8_t *b, int row, int col, int a_zp, int b_zp, int *bias, int *dst) {
+//   for (int c = 0; c < col; ++c) {
+//     for (int r = 0; r < row; ++r) {
+//       int src_idx = r * col + c;
+//       dst[c] += b[src_idx];
+//     }
+//     dst[c] = row * a_zp * b_zp - a_zp * dst[c];
+//     if (bias) {
+//       dst[c] += bias[c];
+//     }
+//   }
+// }
+
+void Row4x2Major2RowMajor(int8_t *src, int row4, int8_t *dst, int row, int cow) {
+  int stride = sizeof(int8_t) * 4 * 2;
+  for (int r = 0; r < row; ++r) {
+    for (int c = 0; c < cow; ++c) {
+      int sride_n = c / 2 * (row4 / 4) + r / 4;
+      int dst_idx = r * cow + c;
+      dst[dst_idx] = src[stride * sride_n + r % 4 * 4 + c % 2];
+    }
+  }
+}
+
+void test() {
+  int8_t a[ROW * DEPTH] = {1, 2, 3, 4};
+  int8_t b[DEPTH * COL] = {1, 1, 1, 1};
+  int a_sums[ROW4] = {0};
+  int b_sums[COL2] = {0};
+
+  int8_t c[ROW4 * COL2] = {0};
+  int8_t output[ROW * COL] = {0};
+  int8_t *a_align = (int8_t *)malloc(ROW4 * DEPTH16);
+  memset(a_align, 0, ROW4 * DEPTH16);
+  int8_t *b_align = (int8_t *)malloc(COL2 * DEPTH16);
+  memset(b_align, 0, COL2 * DEPTH16);
+
+  RowMajor2Row4x16Major(a, ROW, DEPTH, a_align, DEPTH16);
+  RowMajor2Col16x2Major(b, DEPTH, COL, b_align, DEPTH16);
+  // RowMajor2Asums(a, ROW, DEPTH, 0, a_sums);
+  // RowMajor2Bbias(b, DEPTH, COL, 0, 0, NULL, b_sums);
+  int multiplier;
+  int shift;
+  // QuantizeMultiplier(1.0f, &multiplier, &shift);
+
+  MatmulInt8Neon32(a_align, b_align, c, ROW, COL, DEPTH16, a_sums, b_sums, INT_MIN, INT_MAX, 0, multiplier, 0, 0, 0);
+
+#if 0  // test start
+  int8_t c_test[ROW4 * COL4] = {0};
+  Row4x4Major2RowMajor(c, ROW4, COL4, c_test, ROW4, COL4);
+  for (int i = 0; i < ROW4 * COL4; ++i) {
+    printf("%d\t", output[i]);
+    if ((i + 1) % COL4 == 0) printf("\n");
+  }
+  printf("\n");
+  return;
+#endif
+
+  Row4x2Major2RowMajor(c, ROW4, output, ROW, COL);
+  for (int i = 0; i < ROW * COL; ++i) {
+    printf("%d ", output[i]);
+    if ((i + 1) % COL2 == 0) printf("\n");
+  }
+  printf("\n");
+}
+
+int main() {
+  test();
+  return 0;
+}
---
+++ arm32/matmul_int8.s
@@ -0,0 +1,146 @@
+#ifdef __arm__
+#ifndef __aarch64__
+
+.text
+.align 5
+.global MatmulInt8Neon32
+#ifndef __APPLE__
+.type MatmulInt8Neon32, %function
+#endif
+
+
+  //                                              int8 RHS 16x2 block
+  //                                              /-----------------|
+  //                                              |d8.b[0]  d10.b[0]|
+  //                                              |  ...      ...   |
+  //                                              |d8.b[7]  d10.b[7]|
+  //                                              |d9.b[0]  d11.b[0]|
+  //                                              |  ...      ...   |
+  //                                              |d9.b[7]  d11.b[7]|
+  //                                              \-----------------/
+  //    int8 LHS 4x16 block
+  //  /---------------------------------------\   /------------------|
+  //  |d0.b[0] ... d0.b[7] d1.b[0] ... d1.b[7]|   |  q6(q14) q7(q2) |
+  //  |d2.b[0] ... d2.b[7] d3.b[0] ... d3.b[7]|   |  q8(q15) q9(q3) |
+  //  (Reload d0, d1, d2, d3)
+  //  |d0.b[0] ... d0.b[7] d1.b[0] ... d1.b[7]|   |  q10(q14) q11(q2) |
+  //  |d2.b[0] ... d2.b[7] d3.b[0] ... d3.b[7]|   |  q12(q15) q13(q3) |
+  //  \---------------------------------------/   \-----------------/
+  //                                         128-bit accumulators 4x2 block
+  //
+//void MatmulInt8Neon32(const int8_t *a, const int8_t *b, int8_t *dst, int row, int col, int deep16, 
+//                      const int *input_sums, const int *weight_bias, int act_min, int act_max, int out_zp,
+//                      int multiplier, int left_shift, int right_shift, int stride);
+// r0: a, r1: b, r2: dst, r3: row
+// #0: col, #4: deep16, #8: input_sums, #12: weight_bias, #16: act_min, #20: act_max, #24: out_zp
+// #28: multiplier, #32: left_shift, #36: right_shift, #40: stride
+// custom params:
+// #44: a_origin, #48: b_origin, #52: row_origin
+
+MatmulInt8Neon32:
+  push {r4-r11, lr}
+  vpush {q4-q7}
+  add sp, sp, #100    //36+64
+
+  ldr r4, [sp]        // col
+  str r0, [sp, #44]   // origin a
+  str r1, [sp, #48]   // origin b
+  str r3, [sp, #52]   // origin row
+  mov r7, #2
+  ldr r8, [sp, #4]
+  mul r9, r7, r8      // the sride of b
+
+L1:
+  cmp r4, #0    // if at the end of col
+  ble End1
+
+  ldr r0, [sp, #44]   // reload a ptr
+  ldr r3, [sp, #52]   // reset row counter
+  ldr r6, [sp, #8]    // reload intpu_sums ptr
+L2:
+  cmp r3, #0    // if at the end of row
+  ble End2
+
+  ldr r1, [sp, #48]   // reload b ptr
+  ldr r4, [sp, #12]   // reload weight_bias ptr
+  ldr r5, [sp, #4]    // reset deep16
+  vmov.i32 q6, #0
+  vmov.i32 q7, #0
+  vmov.i32 q8, #0
+  vmov.i32 q9, #0
+  vmov.i32 q10, #0
+  vmov.i32 q11, #0
+  vmov.i32 q12, #0
+  vmov.i32 q13, #0
+L3:
+  cmp r3, #0
+  beq End3
+
+  vld1.8 {d0, d1, d2, d3}, [r0]!
+  vld1.8 {d8, d9, d10, d11}, [r1]!
+  vmull.s8 q14, d0, d8
+  vmull.s8 q2, d0, d10
+  vmull.s8 q15, d2, d8
+  vmull.s8 q3, d2, d10
+  vmlal.s8 q14, d1, d9
+  vmlal.s8 q2, d1, d11
+  vmlal.s8 q15, d3, d9
+  vmlal.s8 q3, d3, d11
+
+  vpadal.s16 q6, q14
+  vpadal.s16 q7, q2
+  vpadal.s16 q8, q15
+  vpadal.s16 q9, q3
+
+  vld1.8 {d0, d1, d2, d3}, [r0]!
+  vmull.s8 q14, d0, d8
+  vmull.s8 q2, d0, d10
+  vmull.s8 q15, d2, d8
+  vmull.s8 q3, d2, d10
+  vmlal.s8 q14, d1, d9
+  vmlal.s8 q2, d1, d11
+  vmlal.s8 q15, d3, d9
+  vmlal.s8 q3, d3, d11
+
+  vpadal.s16 q6, q14
+  vpadal.s16 q7, q2
+  vpadal.s16 q8, q15
+  vpadal.s16 q9, q3
+  sub r5, r5, #16  // deep16 -= 16
+  b L3
+
+End3:
+  vpadd.i32 d0, d12, d13
+  vpadd.i32 d1, d14, d15
+  vpadd.i32 d2, d16, d17
+  vpadd.i32 d3, d18, d19
+  vpadd.i32 d4, d20, d21
+  vpadd.i32 d5, d22, d23
+  vpadd.i32 d6, d24, d25
+  vpadd.i32 d7, d26, d27
+
+  vpadd.i32 d28, d0, d1
+  vpadd.i32 d29, d2, d3
+  vpadd.i32 d30, d4, d5
+  vpadd.i32 d31, d6, d7
+
+  //...
+
+  vqmovn.s32 d28, q14
+  vqmovn.s32 d29, q15
+  
+  vst1.16 {q14}, [r2]!
+  sub r3, r3, #4   // a row counter -= 4
+  b L2
+
+End2:
+  sub r4, r4, #2  // b col counter -= 2
+  add r1, r1, r9  // b ptr + stride
+  b L1
+
+End1:
+  sub sp, sp, #100
+  vpop {q4-q7}
+  pop {r4-r11, pc}
+#endif
+#endif
